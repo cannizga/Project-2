8b4ba4a4874a551a6a25b8233d317c6a1a42504c
unknown <giuseppeacannizzojr@gmail.com>
2019-05-01 20:19:31 -0500

    Update

--- Project2/js/scripts.js
+++ Project2/js/scripts.js
@@ -2,12 +2,15 @@ var messages = [];
 
 var prevMessages = ["Hey Man Whats Up", "Hello?", "Are You Ignoring Me?"];
 
-for(i = 0; i < 3; i++)
+function savedMessages() {
+  for (i = 0; i < 3; i++)
   {
-  document.getElementById('UserMessages').innerHTML += '<font color = salmon>' + "-" + "Connor" + ": " + prevMessages[i].toString() + " " + "<br>";
+    document.getElementById('UserMessages').innerHTML += '<font color = salmon>' + "-" + "Connor" + ": " + prevMessages[i].toString() + " " + "<br>";
   }
+}
+savedMessages();
 
-function Message (name1, name2, m2, m, userInput){
+function Message(name1, name2, m2, m, userInput) {
   this.name1 = name1
   this.name2 = name2;
   this.userInput = userInput;
@@ -15,45 +18,41 @@ function Message (name1, name2, m2, m, userInput){
   this.m = m;
 }
 
-function init (nameSwitch) {
-   var user, type;
-   var messageType = {
-   out: 'out-message',
-   in: 'in-message'};
+function init(nameSwitch) {
+  var user, type;
+  var messageType = {
+    out: 'out-message',
+    in: 'in-message'};
 
-   m2 = document.getElementById("message-container");
-   m = m2.elements["name"].value
-   name1 = "Alessandro";
-   name2 = "Connor";
+  m2 = document.getElementById("message-container");
+  m = m2.elements["name"].value
+  name1 = "Alessandro";
+  name2 = "Connor";
 
-   switch (nameSwitch) {
+  switch (nameSwitch) {
 
-     case "send-button":
-     userInput = name1 + ": " + m;
-     type = messageType.out;
-     document.getElementById("message-container").reset();
-     break;
+    case "send-button":
+      userInput = name1 + ": " + m;
+      type = messageType.out;
+      document.getElementById("message-container").reset();
+      break;
 
-     case "reply-button":
-     userInput = name2 + ": " + m;
-     type = messageType.in;
-     document.getElementById("message-container").reset();
-     break;
+    case "reply-button":
+      userInput = name2 + ": " + m;
+      type = messageType.in;
+      document.getElementById("message-container").reset();
+      break;
 
-     default:
-     userInput = "No Message";
-     break;
+    default:
+      userInput = "No Message";
+      break;
+  }
 
-   }
-
-   if (type === 'out-message')
-   {
-   messages.push('<font color = steelblue>' + '-' + userInput + "<br>");
-   document.getElementById('displayMessage').innerHTML = messages.join('');
-   }
-   else if(type === 'in-message')
-   {
-   messages.push('<font color = salmon>' + '-' + userInput + "<br>");
-   document.getElementById('displayMessage').innerHTML = messages.join('');
-   }
+  if (type === 'out-message') {
+    messages.push('<font color = steelblue>' + '-' + userInput + "<br>");
+    document.getElementById('displayMessage').innerHTML = messages.join('');
+  } else if (type === 'in-message') {
+    messages.push('<font color = salmon>' + '-' + userInput + "<br>");
+    document.getElementById('displayMessage').innerHTML = messages.join('');
+  }
 }
